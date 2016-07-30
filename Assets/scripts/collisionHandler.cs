c63c1fecb939c1d14d8da529e41dfad98aebba5f
SirGFM <gfmandaji@hotmail.com>
2016-07-30 09:34:24 -0300

    Implement per-child collision detection

---
+++ Assets/scripts/collisionHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionHandler : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col) {
+		Debug.Log(col.collider.gameObject.name);
+	}
+}
