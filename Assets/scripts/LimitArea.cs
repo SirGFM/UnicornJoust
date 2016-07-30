bd256a485290ab76d2e6300a0fd23c0bcfa1eb4e
SirGFM <gfmandaji@hotmail.com>
2016-07-30 15:41:30 -0300

    Add (bugged) limits to playfield

---
+++ Assets/scripts/LimitArea.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitArea : MonoBehaviour {
+
+	/** Radius of the playable field */
+	static float radius = 8.5f;
+
+	private Rigidbody2D _rb;
+
+	void Awake() {
+		_rb = this.GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		float r2 = LimitArea.radius * LimitArea.radius;
+		if (this.transform.position.sqrMagnitude > r2) {
+			Vector2 norm = this.transform.position.normalized * Time.deltaTime;
+			this.transform.position *= -1.0f;
+			/* Apply a slight force toward the center of the playfield,
+			 * to push it outside the border */
+			this._rb.AddForce(norm);
+		}
+	}
+}
