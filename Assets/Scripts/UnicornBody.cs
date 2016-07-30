c7cfa4903e62af7f8712cdb73fc3ef397d97c02b
Ticolol <tico.amendola1@gmail.com>
2016-07-30 09:22:48 -0300

    FIRST

---
+++ Assets/Scripts/UnicornBody.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnicornBody : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//void OnCollisionEnter(Collision c){
+	void OnTriggerEnter(Collider col){
+		//Collider col = c.collider;
+		if(col.gameObject.name == "Spear"){
+			Debug.Log("AAAAAAAAHHHHHHH");
+		}
+	}
+}
