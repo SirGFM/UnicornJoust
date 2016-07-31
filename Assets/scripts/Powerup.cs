fc245f5b91d1a2648e12607d02b0b95a53fbb4d0
SirGFM <gfmandaji@hotmail.com>
2016-07-31 01:27:54 -0300

    Add powerup spawner and base type for powerups

---
+++ Assets/scripts/Powerup.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+	/** Enumerate the powerup types */
+	public enum Type {
+		POWERUP_NONE = 0,
+		POWERUP_FORCE_FIELD,
+		POWERUP_EXTRA_SPEED,
+		POWERUP_DELAY_ZONE,
+		POWERUP_TRON,
+	}
+
+	/** This powerup's type */
+	public Type type = Type.POWERUP_NONE;
+	/** How long until the powerup disappears */
+	public float time = 15.0f;
+
+	/** Check if no one got the powerup and destroy it */
+	private IEnumerator disappearTimer() {
+		yield return new WaitForSeconds(this.time);
+		/* If this returns, then no one got the powerup (so the object hasn't been destroy) */
+		/* TODO Play animation */
+		GameObject.Destroy(this.gameObject);
+	}
+
+	/** Called after the object gets activated, but only once per script */
+	void Start() {
+		/* Start coroutine to destroy the object if it isn't gotten within a time frame */
+		this.StartCoroutine(this.disappearTimer());
+	}
+
+	void OnEnterCollision2D(Collider2D col) {
+		/* Since the player will handle the collision, simply destroy the powerup */
+		if (col.CompareTag("player")) {
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/scripts/SpawnPowerup.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPowerup : MonoBehaviour {
+
+	/** Minimum time between updates in seconds */
+	public float minTime = 5.0f;
+	/** Maximum time between updates in seconds */
+	public float maxTime = 8.0f;
+	public GameObject[] powerups;
+
+	/** Flag to control polling */
+	private bool _isSpawning;
+
+	/**
+	 * Queue the spawning of a powerup
+	 * 
+	 * @param  [ in]delay How long until the powerup should be spawned
+	 */
+	private IEnumerator spawn(float delay) {
+		int i;
+		/* Wait the desired time and spawn a new powerup */
+		yield return new WaitForSeconds(delay);
+		i = Random.Range(0, powerups.Length);
+		if (i < powerups.Length) {
+			/* TODO Spawn it! */
+		}
+		/* Unqueue the powerup */
+		this._isSpawning = false;
+	}
+
+	/** === UNITY EVENTS ======================================================== */
+
+	/** Called after the object gets activated, but only once per script */
+	void Start () {
+		this._isSpawning = false;
+	}
+
+	/** Called on a variable interval (sync'ed with the draw rate) */
+	void Update () {
+		/* Poll until the next update should be queued */
+		if (!this._isSpawning) {
+			float delay = Random.Range(this.minTime, this.maxTime);
+			this.StartCoroutine(this.spawn(delay));
+			this._isSpawning = true;
+		}
+	}
+}
